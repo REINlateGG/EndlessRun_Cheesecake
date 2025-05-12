c54f5a6731425964d927c0895422886e2d5eae1f
REINlateGG <niratchaporn47@gmail.com>
2025-05-13 03:46:04 +0700

    Add UI HP SCORE MUSIC

--- Assets/Scripts/GameUIManager.cs
+++ Assets/Scripts/GameUIManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameUIManager : MonoBehaviour
 {
@@ -8,11 +9,62 @@ public class GameUIManager : MonoBehaviour
 
     public PlayerController player;
 
+    public GameObject gameOverPanel;
+
+    public AudioSource gameBGM;
+    public Toggle musicToggle;
+
+void Start()
+    {
+        bool musicOn = PlayerPrefs.GetInt("music", 1) == 1;
+
+        if (musicToggle != null)
+        {
+            musicToggle.isOn = musicOn;
+            musicToggle.onValueChanged.AddListener(OnToggleMusic);
+        }
+
+        if (gameBGM != null)
+        {
+            gameBGM.mute = !musicOn;
+        }
+    }
+
     void Update()
     {
         if (!player.gameOver)
         {
             scoreText.text = "Score: " + player.score;
+        }
+    }
+
+     public void ShowGameOver()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+    public void OnPlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnMainMenu()
+    {
+        SceneManager.LoadScene("StartScenes");
+    }
+
+     public void OnExit()
+    {
+        Application.Quit();
+    }
+
+    public void OnToggleMusic(bool on)
+    {
+        if (gameBGM != null)
+        {
+            gameBGM.mute = !on;
+            PlayerPrefs.SetInt("music", on ? 1 : 0);
+            PlayerPrefs.Save();
         }
     }
 }
